d4e65ab1a335f15bb2d04f7d67c6b55bb6864972
Napa <tailangnapa1@gmail.com>
2022-11-10 22:30:03 +0530

    first commint

---
+++ src/components/Basics/MenuApi.js
@@ -0,0 +1,45 @@
+const Menu = [
+    {
+    id:1,
+    image:"images/maggi.avif",
+    name:"maggi",
+    category:"Breakfast",
+    price:25,
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+},
+{
+    id:2,
+    image:"images/chicken3.avif",
+    name:"chicken",
+    category:"lunch",
+    price:25,
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+},
+{
+    id:3,
+    image:"images/fish.avif",
+    name:"fish",
+    category:"dinner",
+    price:25,
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+},
+{
+    id:4,
+    image:"images/fish.avif",
+    name:"fish",
+    category:"Breakfast",
+    price:25,
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+},
+{
+    id:5,
+    image:"images/sweets.jpg",
+    name:"fish",
+    category:"Sweets",
+    price:25,
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+},
+
+
+];
+export default Menu;
